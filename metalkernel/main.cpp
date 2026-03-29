945ee62bb3993ba81c8f4978d0ca555035ea8114
Yussufkadir <ysyurme1@stu.vistula.edu.pl>
2026-03-29 18:38:45 +0200

    Initial Commit

---
+++ metalkernel/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  metalkernel
+//
+//  Created by Yusuf Surmen on 29/03/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
